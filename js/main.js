e72b472091f99f2dfec773ef72e4077e7cc073e8
monilpatel <pmonil@vt.edu>
2017-01-09 16:38:02 -0500

    container fix
    portfolio container fix

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-//$(document).ready(function () {
-//    $('#toggle').click(function () {
-//        $(this).toggleClass('active');
-//        $('#fullnav').addClass('open');
-//        //$('.navbar').toggleClass('off');
-//
-//    });
-//    
-//    
-//    $('#close_menu').click(function(){
-//        $('#fullnav').removeClass('open');
-//    })
-//});
-//
-
-
 $(window).resize(function () {
     var viewportWidth = $(window).width();
     if (viewportWidth > 768) {
@@ -25,7 +9,6 @@ $(window).resize(function () {
 
 
 var app = angular.module("myApp", ["ngRoute"]);
-
 
 app.controller('profileCtrl', function ($scope) {
     $scope.title = "Profile "
@@ -43,19 +26,6 @@ app.controller('profileCtrl', function ($scope) {
         } else {
             $scope.class = "open";
         }
-    }
-
-    $scope.initPhotos = function () {
-//        function getRandomSize(min, max) {
-//            return Math.round(Math.random() * (max - min) + min);
-//        }
-//
-//        for (var i = 0; i < 2; i++) {
-//            var width = getRandomSize(200, 400);
-//            var height = getRandomSize(200, 400);
-//            $('#photos').append('<img src="//www.lorempixel.com/' + width + '/' + height + '/cats" alt="pretty kitty">');
-//        }
-
     }
 
 })
