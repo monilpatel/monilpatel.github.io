7cb3621fd2a16c1303f29d49a06e2cd23374fe26
monilpatel <pmonil@vt.edu>
2017-01-11 15:42:59 -0500

    performance test
    remove smooth scroll to see if scrolling improves

--- js/main.js
+++ js/main.js
@@ -31,12 +31,12 @@ $(window).resize(function () {
     }
 });
 
-
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$('.page-scroll a').bind('click', function (event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-        scrollTop: ($($anchor.attr('href')).offset().top - 50)
-    }, 1250, 'easeInOutExpo');
-    event.preventDefault();
-});
+//
+//// jQuery for page scrolling feature - requires jQuery Easing plugin
+//$('.page-scroll a').bind('click', function (event) {
+//    var $anchor = $(this);
+//    $('html, body').stop().animate({
+//        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+//    }, 1250, 'easeInOutExpo');
+//    event.preventDefault();
+//});
